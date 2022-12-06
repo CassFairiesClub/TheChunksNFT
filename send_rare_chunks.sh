9abc7d4b066a5316f3b3b7e0b077a48933aa8273
CassFairiesClub <115993765+CassFairiesClub@users.noreply.github.com>
2022-12-06 14:51:45 +0100

    Add files via upload
    sending script to rare airdrop winners

---
+++ send_rare_chunks.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+FINGERPRINT=
+WALLETID=
+BLOCK=
+
+wait_transfer() {
+MINT_STATE=$(chia rpc wallet get_wallet_balance '{"wallet_id": 1}' | jq '.wallet_balance.pending_coin_removal_count')
+while [ "$MINT_STATE" -ne 0 ]; do
+sleep 15
+MINT_STATE=$(chia rpc wallet get_wallet_balance '{"wallet_id": 1}' | jq '.wallet_balance.pending_coin_removal_count')
+done
+}
+
+wget https://raw.githubusercontent.com/CassFairiesClub/TheChunksNFT/main/$BLOCK/rare_transfer_list_$BLOCK.txt
+
+for i in $(cat rare_transfer_list_$BLOCK.txt)
+do
+TA=$(echo $i | cut -d ',' -f1)
+NFT_ID=$(echo $i | cut -d ',' -f2)
+NFT_COIN_ID=$(chia rpc wallet nft_get_info '{"wallet_id": '$WALLETID', "coin_id": "'$NFT_ID'"}' | jq -r '.nft_info.nft_coin_id')
+chia wallet nft transfer -f $FINGERPRINT -i $WALLETID -ni $NFT_COIN_ID -ta $TA -m 0.0000000001
+wait_transfer
+done
