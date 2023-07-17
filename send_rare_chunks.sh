183215846344e298d059133cdf96ef4761e7fc77
Cass <contact@cassandre-fairies-club.com>
2023-07-17 10:40:59 +0200

    3888290 commit

--- send_rare_chunks.sh
+++ send_rare_chunks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 FINGERPRINT=3187864955
 WALLETID=5
-BLOCK=3791522
+BLOCK=3856034
 
 wait_transfer() {
 MINT_STATE=$(chia rpc wallet get_wallet_balance '{"wallet_id": 1}' | jq '.wallet_balance.pending_coin_removal_count')
